6476c039062b5654f1f8d1e7406809cb283a92d2
apopp2 <andreas.popp.2@hof-university.de>
2023-06-20 13:57:14 +0200

    Added Ticket number in NavBarGroup

--- src/Components/Stationary/Navigation/NavIconGroup.js
+++ src/Components/Stationary/Navigation/NavIconGroup.js
@@ -3,8 +3,8 @@ import Account from './assets/Account.svg';
 import Warenkorb from './assets/Warenkorb.svg';
 import './Navigation.css';
 
-export default function NavIconGroup() {
-    return (
+export default function NavIconGroup(props) {
+  return (
       <nav className="navIconGroup">
         <div className="navIconGroupItem">
           <img src={Herz}></img>
@@ -12,6 +12,7 @@ export default function NavIconGroup() {
         </div>
         <div className="navIconGroupItem">
           <img src={Warenkorb}></img>
+          {props.itemCount>0 && <div className="navIconGroupItemCount">{props.itemCount}</div>}
           <p>Warenkorb</p>
         </div>
         <div className="navIconGroupItem">
