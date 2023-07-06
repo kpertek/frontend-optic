115436e7344762fcfacd23cb5a009eca8fd35d0d
apopp2 <andreas.popp.2@hof-university.de>
2023-07-07 01:37:36 +0200

    Implemented Popup-Pages

--- src/Components/Stationary/User/AccountPopUp.js
+++ src/Components/Stationary/User/AccountPopUp.js
@@ -1,8 +1,22 @@
+import { useState } from "react";
 import "./AccountPopUp.css";
 import X from "./assets/Close.svg";
 import Login from "./Login";
 import Register from "./Register";
 export default function AccountPopUp() {
+
+    const [content, setContent] = useState("Registrieren");
+
+    const setContentReg  = () =>
+    {
+        setContent("Registrieren");
+    }
+
+    const setContentLogin  = () =>
+    {
+        setContent("Login");
+    }
+
     return (
         <div className="popup">
             <div className="popup_ueberschrift_close">
@@ -14,18 +28,18 @@ export default function AccountPopUp() {
                 </div>
             </div>
             <div className="popup_navbar">
-                <div className="popup_registrieren active">
+                <div className={"popup_registrieren" + (content==="Registrieren" ? " active" : "")} onClick={setContentReg}>
                     Registrieren
                 </div>
-                <div className="popup_anmelden">
+                <div className={"popup_anmelden" + (content==="Login" ? " active" : "")} onClick={setContentLogin}>
                     Anmelden
                 </div>
             </div>
             <div className="login">
-                <Login />
+                {content==="Login" && <Login />}
             </div>
             <div className="register">
-                {/*<Register />*/}
+                {content==="Registrieren" && <Register />}
             </div>
         </div>
     )
